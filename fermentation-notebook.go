1b8fcc949d140e0c37b09ccd347b526f262dc533
Gildas Chabot <gildas.chabot@scmfrance.fr>
2017-04-17 23:53:12 +0200

    Adding Methods

--- fermentation-notebook.go
+++ fermentation-notebook.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -30,6 +31,17 @@ type Event struct {
 }
 
 type Methods struct {
+	Methods []Method
+}
+
+type Method struct {
+	Name  string
+	Steps []Step
+}
+
+type Step struct {
+	Name     string
+	Duration time.Duration
 }
 
 func main() {
@@ -40,8 +52,43 @@ func main() {
 
 	// fmt.Println(bs)
 
-	err = serve(bs)
+	ms, err := readMethods("methods.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// fmt.Println(ms)
+
+	err = serve(bs, ms)
 	fmt.Println(err)
+}
+
+func serve(bs Batches, ms Methods) error {
+	batchesHandler := func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.New("batches.html").Funcs(template.FuncMap{
+			"nl2br": func(s string) template.HTML {
+				return template.HTML(strings.Replace(s, "\n", "<br />\n", -1))
+			}}).ParseFiles("tmpl/batches.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = t.Execute(w, struct {
+			Batches
+			Methods
+		}{bs, ms})
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", batchesHandler).Methods("GET")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+
+	http.Handle("/", r)
+	port := "8080"
+	fmt.Printf("Listening on %s...\n", port)
+	return http.ListenAndServe(":"+port, nil)
 }
 
 func readBatches(input string) (bs Batches, err error) {
@@ -58,26 +105,16 @@ func readBatches(input string) (bs Batches, err error) {
 	return
 }
 
-func serve(bs Batches) error {
-	batchesHandler := func(w http.ResponseWriter, r *http.Request) {
-		t, err := template.New("batches.html").Funcs(template.FuncMap{
-			"nl2br": func(s string) template.HTML {
-				return template.HTML(strings.Replace(s, "\n", "<br />\n", -1))
-			}}).ParseFiles("tmpl/batches.html")
-		if err != nil {
-			fmt.Println(err)
-		}
-		err = t.Execute(w, bs)
-		if err != nil {
-			fmt.Println(err)
-		}
+func readMethods(input string) (ms Methods, err error) {
+	mf, err := os.Open(input)
+	if err != nil {
+		return
+	}
+	mb, err := ioutil.ReadAll(mf)
+	if err != nil {
+		return
 	}
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", batchesHandler).Methods("GET")
-
-	http.Handle("/", r)
-	port := "8080"
-	fmt.Printf("Listening on %s...\n", port)
-	return http.ListenAndServe(":"+port, nil)
+	err = yaml.Unmarshal(mb, &ms)
+	return
 }
