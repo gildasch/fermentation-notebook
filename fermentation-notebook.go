5801fd4cba1cef7be8f45b7f7be14321df185888
Gildas Chabot <gildas.chabot@scmfrance.fr>
2017-04-17 20:16:40 +0200

    Serve HTML to display Batches

--- fermentation-notebook.go
+++ fermentation-notebook.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
 
 	"gopkg.in/yaml.v2"
 )
@@ -33,7 +38,10 @@ func main() {
 		fmt.Println(err)
 	}
 
-	fmt.Println(bs)
+	// fmt.Println(bs)
+
+	err = serve(bs)
+	fmt.Println(err)
 }
 
 func readBatches(input string) (bs Batches, err error) {
@@ -48,4 +56,28 @@ func readBatches(input string) (bs Batches, err error) {
 
 	err = yaml.Unmarshal(bb, &bs)
 	return
+}
+
+func serve(bs Batches) error {
+	batchesHandler := func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.New("batches.html").Funcs(template.FuncMap{
+			"nl2br": func(s string) template.HTML {
+				return template.HTML(strings.Replace(s, "\n", "<br />\n", -1))
+			}}).ParseFiles("tmpl/batches.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = t.Execute(w, bs)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", batchesHandler).Methods("GET")
+
+	http.Handle("/", r)
+	port := "8080"
+	fmt.Printf("Listening on %s...\n", port)
+	return http.ListenAndServe(":"+port, nil)
 }
